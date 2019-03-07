8541ac10b233eed509d546b9d7f3f3323219814b
kenneth-yu <kyu22@buffalo.edu>
2019-03-07 09:51:50 -0500

    posting new messages with error

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,7 +4,7 @@ import ChatCable from "../components/ChatCable";
 
 const MessageList = (props) => {
   const { message, messageData, screenName, chatName, user_id} = props;
-  let allMessages = messageData.map(oneMessage => <MessageItem user_id={user_id} message={oneMessage} className="message-item--other" screenName={chatName}/>)
+  let allMessages = messageData.map(oneMessage => <MessageItem key={oneMessage.id} user_id={user_id} message={oneMessage} className="message-item--other" screenName={chatName}/>)
   return (
     <div className="message-list">
       <div className="message-list__container">
